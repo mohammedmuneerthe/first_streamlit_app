f067b56ec1fbaedf17808938898301d47ea07776
mohammed muneer <110083735+mohammedmuneerthe@users.noreply.github.com>
2022-08-11 20:18:13 +0530

    Update streamlit_app.py
    fruityvice.com

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,3 +18,9 @@ streamlit.dataframe(my_fruit_list)
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+#New section for fruit_load_list
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
