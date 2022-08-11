2c1d6f2af6079e2c739eb3762f3c6726b3619608
mohammed muneer <110083735+mohammedmuneerthe@users.noreply.github.com>
2022-08-11 20:19:28 +0530

    Update streamlit_app.py
    added  text

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,4 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
+streamlit.header("Fruityvice Fruit Advice!")
