93e163cf3d69de6da671b3062bc9de079a252d99
mohammed muneer <110083735+mohammedmuneerthe@users.noreply.github.com>
2022-08-12 16:55:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -85,11 +85,22 @@ if streamlit.button('Get Fruit Load list'):
 	my_data_rows=get_fruit_load_list()
 	streamlit.dataframe(my_data_rows)
 
+##########################
 #adding a fruit insert into table
+#add_my_fruit=streamlit.text_input('What fruit would you like add?')
+
+#streamlit.write('Thanks for adding', add_my_fruit)
+#my_cur.execute("insert into fruit_load_list values ('from streamlit')")  #my_cur.execute("insert into fruit_load_list")
+#####################
+#allow the end user to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+	with my_cnx.cursor() as my_cur:
+		my_cnx.execute("insert into fruit_load_list values ('from streamlit')")
+		return "Thanks for adding" + new_fruit
 add_my_fruit=streamlit.text_input('What fruit would you like add?')
-
-streamlit.write('Thanks for adding', add_my_fruit)
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")  #my_cur.execute("insert into fruit_load_list")
-
+if streamlit.button('Get Fruit Load list'):
+	my_cnx=my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+	back_from_function=insert_row_snowflake(new_fruit)
+	streamlit.dataframe(back_from_function)
 # to stop streamlit
 #streamlit.stop()
