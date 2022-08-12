a501cd5219dd0e5a47fed0f6d991dfac7bbb94b9
mohammed muneer <110083735+mohammedmuneerthe@users.noreply.github.com>
2022-08-12 15:01:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
 import streamlit 
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
+#import streamlit 
 streamlit.title("My parents new healthy Diner")
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -9,7 +15,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 
-import pandas
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index))
@@ -21,7 +27,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #New section for fruit_load_list api
 
-import requests
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
@@ -31,7 +37,7 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 
-import requests
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
 
@@ -42,8 +48,8 @@ streamlit.dataframe(fruityvice_normalized)
 
 #import snowflake.connector
 # to stop streamlit
-streamlit.stop()
-import snowflake.connector
+#streamlit.stop()
+#import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")   #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
@@ -57,4 +63,4 @@ streamlit.write('Thanks for adding', add_my_fruit)
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")  #my_cur.execute("insert into fruit_load_list")
 
 # to stop streamlit
-streamlit.stop()
+#streamlit.stop()
